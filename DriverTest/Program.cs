f597d8e995bc48c8a71471dc77df65921d22e2ec
Curtis Badke <curtis@badkid.ca>
2013-05-17 14:47:54 -0600

    Start hooking in to winmm.dll for midi communications

--- DriverTest/Program.cs
+++ DriverTest/Program.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using NAudio.CoreAudioApi;
 
 namespace DriverTest
 {
@@ -12,29 +11,6 @@ namespace DriverTest
     {
         static void Main(string[] args)
         {
-            var deviceEnum = new MMDeviceEnumerator();
-            var deviceCol = deviceEnum.EnumerateAudioEndPoints(DataFlow.All, DeviceState.All);
-
-            Debugger.Log(1, "output", String.Format("\n=======================================\n"));
-            foreach (var device in deviceCol.Where(d => d.FriendlyName.Contains("PunchLight")))
-            {
-                Debugger.Log(1, "output", String.Format("{0}\n", device.DeviceFriendlyName));
-                Debugger.Log(1, "output", String.Format("{0}\n", device.FriendlyName));
-                Debugger.Log(1, "output", String.Format("\tDataFlow: {0}\n", device.DataFlow));
-                Debugger.Log(1, "output", String.Format("\tState: {0}\n", device.State));
-                Debugger.Log(1, "output", String.Format("\tID: {0}\n", device.ID));
-
-                Debugger.Log(1, "output", String.Format("---------------------------------------\n"));
-            }
-            Debugger.Log(1, "output", String.Format("=======================================\n"));
-
-            var dev = deviceCol.Where(d => d.FriendlyName.Contains("jack4")).First();
-                Debugger.Log(1, "output", String.Format("{0}\n", dev.FriendlyName));
-                Debugger.Log(1, "output", String.Format("\tDataFlow: {0}\n", dev.DataFlow));
-                Debugger.Log(1, "output", String.Format("\tState: {0}\n", dev.State));
-                Debugger.Log(1, "output", String.Format("\tID: {0}\n", dev.ID));
-                //var buff = dev.AudioClient.AudioRenderClient.GetBuffer(1);
-                //dev.AudioClient.AudioRenderClient.ReleaseBuffer(1, AudioClientBufferFlags.Silent);
         }
     }
 }
--- PunchyAPI/CommandBuilder.fs
+++ PunchyAPI/CommandBuilder.fs
@@ -1,33 +1,33 @@
 ﻿module private CommandBuilder
 
-        open System
+    open System
 
-        let MessageStart = seq [ 0xF0uy; 0x7Euy; 0x00uy; 0x07uy; 0x02uy; 0x01uy; 0x00uy; 0x03uy ]
-        let MessageEnd = seq [ 0xF7uy; ]
+    let MessageStart = seq [ 0xF0uy; 0x7Euy; 0x00uy; 0x07uy; 0x02uy; 0x01uy; 0x00uy; 0x03uy ]
+    let MessageEnd = seq [ 0xF7uy; ]
 
-        type Command =
-                | Brightness = 0x02uy
-                | Trigger = 0x08uy
-                | Color = 0x40uy
+    type Command =
+        | Brightness = 0x02uy
+        | Trigger = 0x08uy
+        | Color = 0x40uy
 
-        type ColourChannel =
-                | Red = 0x00uy
-                | Green = 0x10uy
-                | Blue = 0x20uy
+    type ColourChannel =
+        | Red = 0x00uy
+        | Green = 0x10uy
+        | Blue = 0x20uy
 
-        type LightIndex =
-                | Record = 0x00uy
-                | Ready = 0x01uy
-                | Cue = 0x02uy
-                | Note1 = 0x03uy
-                | Note2 = 0x04uy
+    type LightIndex =
+        | Record = 0x00uy
+        | Ready = 0x01uy
+        | Cue = 0x02uy
+        | Note1 = 0x03uy
+        | Note2 = 0x04uy
 
-        type TriggerIndex =
-                | Off = 0x00uy
-                | Ready = 0x01uy
-                | Record = 0x02uy
-                | Cue = 0x03uy
+    type TriggerIndex =
+        | Off = 0x00uy
+        | Ready = 0x01uy
+        | Record = 0x02uy
+        | Cue = 0x03uy
 
-        let ConvertToHalfByte (value : byte) =
-                let valueToConvert = if value > 0xFDuy then 0xFDuy else value
-                0x80uy - Convert.ToByte(Math.Ceiling((float)(valueToConvert + 0x01uy)/2.0))
+    let ConvertToHalfByte (value : byte) =
+        let valueToConvert = if value > 0xFDuy then 0xFDuy else value
+        0x80uy - Convert.ToByte(Math.Ceiling((float)(valueToConvert + 0x01uy)/2.0))
