cad02350a2c91219deb6f9f39aba3d423482f5f7
Curtis Badke <curtis@badkid.ca>
2013-05-18 12:07:06 -0600

    Hide all unmanaged interop in single module.

--- PunchyAPI/NativeMidi.fs
+++ PunchyAPI/NativeMidi.fs
@@ -1,12 +1,12 @@
-﻿namespace Punchy.Native
+﻿namespace Punchy
 
-    module internal Midi =
+    module internal Native =
 
         open System
         open System.Runtime.InteropServices
         open Microsoft.FSharp.NativeInterop
 
-        type MMRESULT =
+        type private MMRESULT =
             | Success = 0
             | Error = 1
             | BadDeviceId = 2
@@ -19,7 +19,7 @@
             | BadErrorNumber = 9
             | InvalidFlag = 10
             | InvalidParameter = 11
-            | HandleBudy = 12
+            | HandleBusy = 12
             | InvalidAlias = 13
             | BadDb = 14
             | KeyNotFound = 15
@@ -33,7 +33,7 @@
             | WaveUnprepared = 34
 
         [<StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)>]
-        type MIDIOUTCAPS =
+        type private MIDIOUTCAPS =
             struct
                 val mutable wMid : UInt16
                 val mutable wPid : UInt16
@@ -46,10 +46,10 @@
                 val mutable wChannelMask : UInt16
                 val mutable dwSupport : UInt32
             end
-        let MIDIOUTCAPS_SIZE = uint32 (Marshal.SizeOf(typeof<MIDIOUTCAPS>))
+        let private MIDIOUTCAPS_SIZE = uint32 (Marshal.SizeOf(typeof<MIDIOUTCAPS>))
 
         [<StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)>]
-        type MIDIHDR =
+        type private MIDIHDR =
             struct
                 val mutable lpData : IntPtr
                 val mutable dwBufferLength : UInt32
@@ -63,45 +63,94 @@
                 [<MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)>]
                 val mutable dwReserved : byte array
             end
-        let MIDIHDR_SIZE = 48u
+        let private MIDIHDR_SIZE = 48u
 
-        type HMIDIOUT = IntPtr
+        type private HMIDIOUT = IntPtr
 
-        [<DllImport(@"winmm.dll", EntryPoint="midiOutGetNumDevs")>]
-        extern UInt32 midiOutGetNumDevs()
+        module private Import =
+            [<DllImport(@"winmm.dll", EntryPoint="midiOutGetNumDevs")>]
+            extern UInt32 midiOutGetNumDevs()
 
-        [<DllImport(@"winmm.dll", EntryPoint="midiOutGetDevCaps")>]
-        extern MMRESULT midiOutGetDevCaps(
-            UIntPtr uDeviceID,      
-            MIDIOUTCAPS& lpMidiOutCaps,  
-            UInt32 cbMidiOutCaps);
+            [<DllImport(@"winmm.dll", EntryPoint="midiOutGetDevCaps")>]
+            extern MMRESULT midiOutGetDevCaps(
+                UIntPtr uDeviceID,
+                MIDIOUTCAPS& lpMidiOutCaps,
+                UInt32 cbMidiOutCaps);
 
-        [<DllImport(@"winmm.dll", EntryPoint="midiOutOpen")>]
-        extern MMRESULT midiOutOpen(
-            HMIDIOUT& lphmo,
-            UInt32 uDeviceID,
-            IntPtr dwCallback,
-            IntPtr dwCallbackInstance,
-            UInt32 dwFlags);
+            [<DllImport(@"winmm.dll", EntryPoint="midiOutOpen")>]
+            extern MMRESULT midiOutOpen(
+                HMIDIOUT& lphmo,
+                UInt32 uDeviceID,
+                IntPtr dwCallback,
+                IntPtr dwCallbackInstance,
+                UInt32 dwFlags);
 
-        [<DllImport(@"winmm.dll", EntryPoint="midiOutClose")>]
-        extern MMRESULT midiOutClose(
-            HMIDIOUT hmo);
+            [<DllImport(@"winmm.dll", EntryPoint="midiOutClose")>]
+            extern MMRESULT midiOutClose(
+                HMIDIOUT hmo);
 
-        [<DllImport(@"winmm.dll", EntryPoint="midiOutPrepareHeader")>]
-        extern MMRESULT midiOutPrepareHeader(
-            HMIDIOUT hmo,
-            MIDIHDR& lpMidiOutHdr,
-            UInt32 cbMidiOutHdr);
+            [<DllImport(@"winmm.dll", EntryPoint="midiOutPrepareHeader")>]
+            extern MMRESULT midiOutPrepareHeader(
+                HMIDIOUT hmo,
+                MIDIHDR& lpMidiOutHdr,
+                UInt32 cbMidiOutHdr);
 
-        [<DllImport(@"winmm.dll", EntryPoint="midiOutUnprepareHeader")>]
-        extern MMRESULT midiOutUnprepareHeader(
-            HMIDIOUT hmo,
-            MIDIHDR& lpMidiOutHdr,
-            UInt32 cbMidiOutHdr);
+            [<DllImport(@"winmm.dll", EntryPoint="midiOutUnprepareHeader")>]
+            extern MMRESULT midiOutUnprepareHeader(
+                HMIDIOUT hmo,
+                MIDIHDR& lpMidiOutHdr,
+                UInt32 cbMidiOutHdr);
 
-        [<DllImport(@"winmm.dll", EntryPoint="midiOutLongMsg")>]
-        extern MMRESULT midiOutLongMsg(
-            HMIDIOUT hmo,
-            MIDIHDR& lpMidiOutHdr,
-            UInt32 cbMidiOutHdr);
+            [<DllImport(@"winmm.dll", EntryPoint="midiOutLongMsg")>]
+            extern MMRESULT midiOutLongMsg(
+                HMIDIOUT hmo,
+                MIDIHDR& lpMidiOutHdr,
+                UInt32 cbMidiOutHdr);
+
+        let AvailableDevices() =
+            let Connectable id =
+                let mutable handle = IntPtr.Zero
+                match Import.midiOutOpen(&handle, id, IntPtr.Zero, IntPtr.Zero, 0u) with
+                | MMRESULT.Success ->
+                    Import.midiOutClose(handle) |> ignore
+                    true
+                | _ -> false
+            let deviceCount = Import.midiOutGetNumDevs()
+            let names = seq {
+                for i in 1u .. deviceCount do
+                    let mutable caps = new MIDIOUTCAPS()
+                    match Import.midiOutGetDevCaps(UIntPtr (i-1u), &caps, MIDIOUTCAPS_SIZE) with
+                    | MMRESULT.Success -> yield (i-1u, caps.szPname)
+                    | _ -> ();
+            }
+            names
+            |> Seq.filter (fun x -> (snd x) = "MIDIOUT2 (PunchLight USB)")
+            |> Seq.filter (fun x -> Connectable (fst x))
+            |> Seq.map fst
+            |> Seq.toList
+
+        let SendData data id =
+            let mutable handle = IntPtr.Zero
+            match Import.midiOutOpen(&handle, id, IntPtr.Zero, IntPtr.Zero, 0u) with
+            | MMRESULT.Success ->
+                let mutable packet = new MIDIHDR()
+                let dp = Marshal.AllocCoTaskMem(Array.length data)
+
+                for i in 0 .. ((Array.length data) - 1) do
+                    let t = IntPtr(dp.ToInt32() + i)
+                    Marshal.StructureToPtr(data.[i], t, false)
+
+                packet.lpData <- IntPtr (dp.ToPointer())
+                packet.dwBufferLength <- uint32 (Array.length data)
+                packet.dwBytesRecorded <- uint32 (Array.length data)
+                packet.dwUser <- IntPtr.Zero
+
+                Import.midiOutPrepareHeader(handle, &packet, MIDIHDR_SIZE) |> ignore
+                Import.midiOutLongMsg(handle, &packet, MIDIHDR_SIZE) |> ignore
+                Import.midiOutUnprepareHeader(handle, &packet, MIDIHDR_SIZE) |> ignore
+
+                Marshal.FreeCoTaskMem(dp)
+
+                Import.midiOutClose(handle) |> ignore
+                true
+            | _ -> false
