2c7b3c733fc72f5b49b5e7c747d5853ee3912491
Curtis Badke <curtis@badkid.ca>
2013-05-17 20:42:51 -0600

    start module to wrap native interop

--- PunchyAPI/NativeMidi.fs
+++ PunchyAPI/NativeMidi.fs
@@ -1,70 +1,73 @@
-﻿module private NativeMidi
+﻿namespace Punchy.Native
 
-    open System
-    open System.Runtime.InteropServices
-    open Microsoft.FSharp.NativeInterop
+    module internal Midi =
 
-    type MMRESULT =
-        | Success = 0
-        | Error = 1
-        | BadDeviceId = 2
-        | NotEnabled = 3
-        | Allocated = 4
-        | InvalidHandle = 5
-        | NoDriver = 6
-        | NoMemory = 7
-        | NotSupported = 8
-        | BadErrorNumber = 9
-        | InvalidFlag = 10
-        | InvalidParameter = 11
-        | HandleBudy = 12
-        | InvalidAlias = 13
-        | BadDb = 14
-        | KeyNotFound = 15
-        | ReadError = 16
-        | WriteError = 17
-        | DeleteError = 18
-        | ValueNotFound = 19
-        | NoDriverCb= 20
-        | BadWaveFormat = 32
-        | WaveStillPlaying = 33
-        | WaveUnprepared = 34
+        open System
+        open System.Runtime.InteropServices
+        open Microsoft.FSharp.NativeInterop
 
-    [<StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)>]
-    type MIDIOUTCAPS =
-        struct
-            val mutable wMid : UInt16
-            val mutable wPid : UInt16
-            val mutable vDriverVersion : UInt32
-            [<MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)>]
-            val mutable szPname : String
-            val mutable wTechnology : UInt16
-            val mutable wVoices : UInt16
-            val mutable wNotes : UInt16
-            val mutable wChannelMask : UInt16
-            val mutable dwSupport : UInt32
-        end
-    
-    let MIDIOUTCAPS_SIZE = uint32 (Marshal.SizeOf(typeof<MIDIOUTCAPS>))
+        type MMRESULT =
+            | Success = 0
+            | Error = 1
+            | BadDeviceId = 2
+            | NotEnabled = 3
+            | Allocated = 4
+            | InvalidHandle = 5
+            | NoDriver = 6
+            | NoMemory = 7
+            | NotSupported = 8
+            | BadErrorNumber = 9
+            | InvalidFlag = 10
+            | InvalidParameter = 11
+            | HandleBudy = 12
+            | InvalidAlias = 13
+            | BadDb = 14
+            | KeyNotFound = 15
+            | ReadError = 16
+            | WriteError = 17
+            | DeleteError = 18
+            | ValueNotFound = 19
+            | NoDriverCb= 20
+            | BadWaveFormat = 32
+            | WaveStillPlaying = 33
+            | WaveUnprepared = 34
 
+        [<StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)>]
+        type MIDIOUTCAPS =
+            struct
+                val mutable wMid : UInt16
+                val mutable wPid : UInt16
+                val mutable vDriverVersion : UInt32
+                [<MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)>]
+                val mutable szPname : String
+                val mutable wTechnology : UInt16
+                val mutable wVoices : UInt16
+                val mutable wNotes : UInt16
+                val mutable wChannelMask : UInt16
+                val mutable dwSupport : UInt32
+            end
+        
+        let MIDIOUTCAPS_SIZE = uint32 (Marshal.SizeOf(typeof<MIDIOUTCAPS>))
 
-    [<DllImport(@"winmm.dll", EntryPoint="midiOutGetNumDevs")>]
-    extern UInt32 midiOutGetNumDevs()
+        type Handle = IntPtr
 
-    [<DllImport(@"winmm.dll", EntryPoint="midiOutGetDevCaps")>]
-    extern MMRESULT midiOutGetDevCaps(
-        UIntPtr uDeviceID,      
-        MIDIOUTCAPS& lpMidiOutCaps,  
-        UInt32 cbMidiOutCaps);
+        [<DllImport(@"winmm.dll", EntryPoint="midiOutGetNumDevs")>]
+        extern UInt32 midiOutGetNumDevs()
 
-    [<DllImport(@"winmm.dll", EntryPoint="midiOutOpen")>]
-    extern MMRESULT midiOutOpen(
-        IntPtr& lphmo,
-        UInt32 uDeviceID,
-        IntPtr dwCallback,
-        IntPtr dwCallbackInstance,
-        UInt32 dwFlags);
+        [<DllImport(@"winmm.dll", EntryPoint="midiOutGetDevCaps")>]
+        extern MMRESULT midiOutGetDevCaps(
+            UIntPtr uDeviceID,      
+            MIDIOUTCAPS& lpMidiOutCaps,  
+            UInt32 cbMidiOutCaps);
 
-    [<DllImport(@"winmm.dll", EntryPoint="midiOutClose")>]
-    extern MMRESULT midiOutClose(
-        IntPtr hmo);
+        [<DllImport(@"winmm.dll", EntryPoint="midiOutOpen")>]
+        extern MMRESULT midiOutOpen(
+            Handle& lphmo,
+            UInt32 uDeviceID,
+            IntPtr dwCallback,
+            IntPtr dwCallbackInstance,
+            UInt32 dwFlags);
+
+        [<DllImport(@"winmm.dll", EntryPoint="midiOutClose")>]
+        extern MMRESULT midiOutClose(
+            Handle hmo);
---
+++ PunchyAPI/NativeWrapper.fs
@@ -0,0 +1,24 @@
+﻿namespace Punchy.Native
+    module internal Wrapper =
+
+        open System
+        open Midi
+
+        let Devices() =
+            let Connectable id =
+                let mutable handle = IntPtr.Zero
+                match midiOutOpen(&handle, id, IntPtr.Zero, IntPtr.Zero, 0u) with
+                | MMRESULT.Success -> 
+                    midiOutClose(handle) |> ignore
+                    true
+                | _ -> false
+            let deviceCount = midiOutGetNumDevs()
+            let names = seq {
+                for i in 1u .. deviceCount do
+                    let mutable caps = new MIDIOUTCAPS()
+                    match midiOutGetDevCaps(UIntPtr (i-1u), &caps, MIDIOUTCAPS_SIZE) with
+                    | MMRESULT.Success -> yield (i-1u, caps.szPname)
+                    | _ -> ();
+            }
+            names |> Seq.filter (fun x -> Connectable (fst x)) |> Seq.toList
+
